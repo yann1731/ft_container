dc5a98876db95e6b8858d3fa86088f9b233e8aee
yannick <yan_1731@hotmail.com>
2023-01-22 15:37:24 -0500

    let's go

--- vector.hpp
+++ vector.hpp
@@ -6,9 +6,11 @@ template <typename T, class Allocator = std::allocator<T> >
 class vector
 {
 public:
-    
+    vector() {_elem = Allocator.allocate(0);};
+    vector(const vector &otherVector){*this = otherVector;};
+    ~vector(_elem Allocator.deallocate());
 protected:
-
+    
 private:
     T *_elem;
     size_t _size;
