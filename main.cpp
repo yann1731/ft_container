71236436f149861805ad75782aca259fc2199231
Yannick St-laurent <yst-laur@c1r8p4.42quebec.com>
2023-01-31 13:33:14 -0500

    let's go

--- main.cpp
+++ main.cpp
@@ -2,21 +2,9 @@
 #include <stack>
 #include "vector.hpp"
 
-class Whatever
-{
-    void amipublic(void)
-    {
-        std::cout << "I am public apparently. wtf" << std::endl;
-    };
-    public:
-    Whatever();
-    ~Whatever();
-};
-
 int main(void)
 {
-    Whatever we;
+    ft::vector<int> vec;
 
-    we.amipublic();
     return 0;
 }
--- vector.hpp
+++ vector.hpp
@@ -7,9 +7,29 @@
 namespace ft
 {
 
+template <typename T, class Allocator>
+
+class _vector_base
+{
+public:
+    typedef Allocator                              	 allocator_type;
+    typedef std::allocator_traits<allocator_type>    __alloc_traits;
+    typedef typename __alloc_traits::size_type       size_type;
+protected:
+    typedef T                                     	 value_type;
+    typedef value_type&                              reference;
+    typedef const value_type&                        const_reference;
+    typedef typename __alloc_traits::difference_type difference_type;
+    typedef typename __alloc_traits::pointer         pointer;
+    typedef typename __alloc_traits::const_pointer   const_pointer;
+    typedef pointer                                  iterator;
+    typedef const_pointer                            const_iterator;
+
+};
+
 template <typename T, class Allocator = std::allocator<T> >
 
-class vector
+class vector: public _vector_base
 {
 protected:
 	typedef T value_type;
@@ -20,10 +40,14 @@ protected:
 	typedef const value_type& const_reference;
 	typedef typename std::allocator_traits<Allocator>::pointer pointer;
 	typedef typename std::allocator_traits<Allocator>::const_pointer const_pointer;
-	typedef value_type* iterator;
-	typedef const value_type* const_iterator;
+	typedef std::iterator<std::random_access_iterator_tag, value_type, difference_type, value_type*, value_type&> iterator;
+	typedef const std::iterator<std::random_access_iterator_tag, const value_type, difference_type, const value_type*, const value_type&> const_iterator;
 	typedef std::reverse_iterator<iterator> reverse_iterator;
 	typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
+
+	pointer start_;
+	pointer end_;
+	pointer last_;
 
 public:
 	vector()
