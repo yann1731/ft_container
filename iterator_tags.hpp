f539f45d6dbeb3c01f29fd9eddfc4a98a2438aa7
yannick <yan_1731@hotmail.com>
2023-02-07 10:54:19 -0500

    let's go

--- iterator_tags.hpp
+++ iterator_tags.hpp
@@ -2,27 +2,27 @@ namespace ft
 {
 
 
-class output_iterator_tag
+struct output_iterator_tag
 {
 
 };
 
-class input_iterator_tag: public output_iterator_tag
+struct input_iterator_tag: public output_iterator_tag
 {
 
 };
 
-class forward_iterator_tag: public input_iterator_tag
+struct forward_iterator_tag: public input_iterator_tag
 {
 
 };
 
-class bidirection_iterator_tag: public forward_iterator_tag
+struct bidirection_iterator_tag: public forward_iterator_tag
 {
 
 };
 
-class random_access_iterato_tag: public bidirection_iterator_tag
+struct random_access_iterator_tag: public bidirection_iterator_tag
 {
 
 };
--- iterator_trait.hpp
+++ iterator_trait.hpp
@@ -2,6 +2,8 @@
 namespace ft
 {
 
+template<typename Iter>
+    struct iterator_traits;
 
 template <class Iter>
 struct iterator_traits
@@ -16,13 +18,21 @@ struct iterator_traits
 template <class T>
 struct iterator_traits<T*>
 {
-
+	typedef ft::random_access_iterator_tag	iterator_category;
+    typedef T                         		value_type;
+    typedef ptrdiff_t                   	difference_type;
+    typedef T*                        		pointer;
+    typedef T&                        		reference;
 };
 
 template <class T>
 struct iterator_traits<const T*>
 {
-
+	typedef ft::random_access_iterator_tag	iterator_category;
+    typedef T                         		value_type;
+    typedef ptrdiff_t                   	difference_type;
+    typedef T*                        		pointer;
+    typedef T&                        		reference;
 };	
 
 
