39616e1745b1b9ebf3133e2b061476b833496d3a
yannick <yan_1731@hotmail.com>
2023-02-07 10:07:26 -0500

    Prolly incomplete but who knows

---
+++ enable_if.hpp
@@ -0,0 +1,17 @@
+#include <cstddef>
+
+namespace ft
+{
+
+template <bool B, class T = void>
+struct enable_if
+{
+};
+
+template <typename T>
+struct enable_if<true, T>
+{
+    typedef T type;
+};
+
+}
