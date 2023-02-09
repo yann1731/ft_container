714536b0e68856092e8f49d6cf829b58f58f8310
fenix144 <yan_1731@hotmail.com>
2023-02-09 17:04:45 -0500

    Still needs some work

--- ref/vector_ref.hpp
+++ ref/vector_ref.hpp
@@ -101,19 +101,18 @@ namespace std
         typedef __gnu_cxx::__alloc_traits<_Tp_alloc_type>	_Alloc_traits;
 
     public:
-        typedef _Tp					value_type;
-        typedef typename _Base::pointer			pointer;
-        typedef typename _Alloc_traits::const_pointer	const_pointer;
-        typedef typename _Alloc_traits::reference		reference;
-        typedef typename _Alloc_traits::const_reference	const_reference;
-        typedef __gnu_cxx::__normal_iterator<pointer, vector> iterator;
-        typedef __gnu_cxx::__normal_iterator<const_pointer, vector>
-        const_iterator;
-        typedef std::reverse_iterator<const_iterator>	const_reverse_iterator;
-        typedef std::reverse_iterator<iterator>		reverse_iterator;
-        typedef size_t					size_type;
-        typedef ptrdiff_t					difference_type;
-        typedef _Alloc					allocator_type;
+        typedef _Tp					                                          value_type;
+        typedef typename _Base::pointer			                          pointer;
+        typedef typename _Alloc_traits::const_pointer	                const_pointer;
+        typedef typename _Alloc_traits::reference		                  reference;
+        typedef typename _Alloc_traits::const_reference	              const_reference;
+        typedef __gnu_cxx::__normal_iterator<pointer, vector>         iterator;
+        typedef __gnu_cxx::__normal_iterator<const_pointer, vector>   const_iterator;
+        typedef std::reverse_iterator<const_iterator>	                const_reverse_iterator;
+        typedef std::reverse_iterator<iterator>		                    reverse_iterator;
+        typedef size_t					                                      size_type;
+        typedef ptrdiff_t					                                    difference_type;
+        typedef _Alloc					                                      allocator_type;
 
     private:
 
