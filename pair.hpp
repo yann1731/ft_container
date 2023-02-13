7092e4d11997f2f8ffa95a831c0262e10dbb9074
yannick <yan_1731@hotmail.com>
2023-02-13 16:42:38 -0500

    Implement make_pair

--- pair.hpp
+++ pair.hpp
@@ -1,6 +1,9 @@
 namespace ft
 {
-template <class T1, class T2>
+template<class T1, class T2>
+    pair<T1, T2> make_pair(T1 t, T2 u) {return pair<T1, T2>(t, u)};
+
+template<class T1, class T2>
 struct pair
 {
     T1 first;
@@ -10,7 +13,7 @@ struct pair
 
     pair(const T1& x, const T2& y): first(x), second(y) {};
 
-template <class U1, class U2>
+template<class U1, class U2>
     pair(const pair<U1, U2>& p): first(p.first), second(p.second) {};
 
     pair& operator=(const pair& other) {this->first = other.first; this->second = other.second; return *this;}
@@ -34,7 +37,7 @@ template<class T1, class T2>
         return (lhs.first < rhs.first || (!(rhs.first < lhs.first) && lhs.second < rhs.second));
     }
 
-template <class T1, class T2>
+template<class T1, class T2>
     bool operator<=(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
     {
         return !(rhs < lhs);
