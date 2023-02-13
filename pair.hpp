6039023d31765b3f2e9cf7dba6a2e878474a6af3
yannick <yan_1731@hotmail.com>
2023-02-13 16:35:46 -0500

    Implement pair. Just need to do make_pair

--- pair.hpp
+++ pair.hpp
@@ -1,9 +1,53 @@
 namespace ft
 {
 template <class T1, class T2>
-class pair
+struct pair
 {
-public:
-	
+    T1 first;
+    T2 second;
+
+    pair(): first(), second() {};
+
+    pair(const T1& x, const T2& y): first(x), second(y) {};
+
+template <class U1, class U2>
+    pair(const pair<U1, U2>& p): first(p.first), second(p.second) {};
+
+    pair& operator=(const pair& other) {this->first = other.first; this->second = other.second; return *this;}
 };
+
+template<class T1, class T2>
+    bool operator==(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+    {
+        return (lhs.first == rhs.first && lhs.second == lhs.second);
+    }
+
+template<class T1, class T2>
+    bool operator!=(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+    {
+        return !(lhs == rhs);
+    }
+
+template<class T1, class T2>
+    bool operator<(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+    {
+        return (lhs.first < rhs.first || (!(rhs.first < lhs.first) && lhs.second < rhs.second));
+    }
+
+template <class T1, class T2>
+    bool operator<=(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+    {
+        return !(rhs < lhs);
+    }
+
+template<class T1, class T2>
+    bool operator>(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+    {
+        return (rhs < lhs);
+    }
+template<class T1, class T2>
+    bool operator>=(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+    {
+        return !(lhs < rhs);
+    }
 }
