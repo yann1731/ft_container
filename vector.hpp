110dd2a3987c0132e5ecd04de732dc854ac08e2f
Yannick St-laurent <yst-laur@c1r8p4.42quebec.com>
2023-02-15 19:28:03 -0500

    let's go

--- vector.hpp
+++ vector.hpp
@@ -17,20 +17,16 @@ namespace ft
 		typedef typename allocator_type::difference_type 			difference_type;
 		typedef typename allocator_type::pointer 					pointer;
 		typedef typename allocator_type::const_pointer 				const_pointer;
-		typedef vector_iterator<pointer, vector<T> > 				iterator;
-		typedef vector_iterator<const_pointer, vector<T> > 			const_iterator;
-		typedef reverse_iterator<iterator> 							reverse_iterator;
-		typedef reverse_iterator<const_iterator> 					const_reverse_iterator;
+		typedef ft::vector_iterator<pointer, vector<T> > 			iterator;
+		typedef ft::vector_iterator<const_pointer, vector<T> > 		const_iterator;
+		typedef ft::reverse_iterator<iterator> 						reverse_iterator;
+		typedef ft::reverse_iterator<const_iterator> 				const_reverse_iterator;
 
 	private:
 		allocator_type 	_alloc;
 		pointer 		_begin;
 		pointer 		_last;
 		pointer 		_end;
-		alloc_range(size_type count, const T& value = T()) {
-			_begin = _alloc.allocate(count);
-			_alloc.construct(_begin, value);
-		}
 	
 	public:
 		vector(): _alloc(Allocator()) {};
